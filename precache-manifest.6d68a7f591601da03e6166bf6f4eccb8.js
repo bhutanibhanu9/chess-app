bc9c2c40e83da3cb87f0c3df951df001b98078ef
bhutanibhanu <bhutanibhanu@gmail.com>
2022-02-26 17:47:34 +0530

    Updates

---
+++ precache-manifest.6d68a7f591601da03e6166bf6f4eccb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cb48f955cd2ac3ccf69",
+    "url": "/chess_app/static/css/main.4f57658c.chunk.css"
+  },
+  {
+    "revision": "8cb48f955cd2ac3ccf69",
+    "url": "/chess_app/static/js/main.8cb48f95.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/chess_app/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "45de306f3f336216fdf0",
+    "url": "/chess_app/static/js/2.45de306f.chunk.js"
+  },
+  {
+    "revision": "6c7e04041d7777f75887",
+    "url": "/chess_app/static/js/runtime~main.6c7e0404.js"
+  },
+  {
+    "revision": "9a5f00988bde948af029b74ae92f4b52",
+    "url": "/chess_app/index.html"
+  }
+];
