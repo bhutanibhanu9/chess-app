78c6c07d0822490d0c7193324652fb1e4707d987
bhanu <bhanu@bhanus-MacBook-Air.local>
2022-03-12 15:41:58 +0530

    Reafactoring Code

---
+++ src/Constants.ts
@@ -0,0 +1,327 @@
+export const VERTICAL_AXIS = ["1", "2", "3", "4", "5", "6", "7", "8"];
+export const HORIZONTAL_AXIS = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+export const GRID_SIZE = 80;
+
+export function samePosition(p1: Position, p2: Position) {
+  return p1.x === p2.x && p1.y === p2.y;
+}
+
+export interface Position {
+  x: number;
+  y: number;
+}
+
+export enum PieceType {
+  PAWN,
+  BISHOP,
+  KNIGHT,
+  ROOK,
+  QUEEN,
+  KING,
+}
+
+export enum TeamType {
+  OPPONENT,
+  OUR,
+}
+
+export interface Piece {
+  image: string;
+  position: Position;
+  type: PieceType;
+  team: TeamType;
+  enPassant?: boolean;
+}
+
+export const initialBoardState: Piece[] = [
+  {
+    image: `assets/images/black-rook.png`,
+    position: {
+      x: 0,
+      y: 7,
+    },
+    type: PieceType.ROOK,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-knight.png`,
+    position: {
+      x: 1,
+      y: 7,
+    },
+    type: PieceType.KNIGHT,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-bishop.png`,
+    position: {
+      x: 2,
+      y: 7,
+    },
+    type: PieceType.BISHOP,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-queen.png`,
+    position: {
+      x: 3,
+      y: 7,
+    },
+    type: PieceType.QUEEN,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-king.png`,
+    position: {
+      x: 4,
+      y: 7,
+    },
+    type: PieceType.KING,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-bishop.png`,
+    position: {
+      x: 5,
+      y: 7,
+    },
+    type: PieceType.BISHOP,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-knight.png`,
+    position: {
+      x: 6,
+      y: 7,
+    },
+    type: PieceType.KNIGHT,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-rook.png`,
+    position: {
+      x: 7,
+      y: 7,
+    },
+    type: PieceType.ROOK,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-pawn.png`,
+    position: {
+      x: 0,
+      y: 6,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-pawn.png`,
+    position: {
+      x: 1,
+      y: 6,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-pawn.png`,
+    position: {
+      x: 2,
+      y: 6,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-pawn.png`,
+    position: {
+      x: 3,
+      y: 6,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-pawn.png`,
+    position: {
+      x: 4,
+      y: 6,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-pawn.png`,
+    position: {
+      x: 5,
+      y: 6,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-pawn.png`,
+    position: {
+      x: 6,
+      y: 6,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OPPONENT,
+  },
+  {
+    image: `assets/images/black-pawn.png`,
+    position: {
+      x: 7,
+      y: 6,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OPPONENT,
+  },
+
+  {
+    image: `assets/images/white-rook.png`,
+    position: {
+      x: 0,
+      y: 0,
+    },
+    type: PieceType.ROOK,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-knight.png`,
+    position: {
+      x: 1,
+      y: 0,
+    },
+    type: PieceType.KNIGHT,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-bishop.png`,
+    position: {
+      x: 2,
+      y: 0,
+    },
+    type: PieceType.BISHOP,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-queen.png`,
+    position: {
+      x: 3,
+      y: 0,
+    },
+    type: PieceType.QUEEN,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-king.png`,
+    position: {
+      x: 4,
+      y: 0,
+    },
+    type: PieceType.KING,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-bishop.png`,
+    position: {
+      x: 5,
+      y: 0,
+    },
+    type: PieceType.BISHOP,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-knight.png`,
+    position: {
+      x: 6,
+      y: 0,
+    },
+    type: PieceType.KNIGHT,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-rook.png`,
+    position: {
+      x: 7,
+      y: 0,
+    },
+    type: PieceType.ROOK,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-pawn.png`,
+    position: {
+      x: 0,
+      y: 1,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-pawn.png`,
+    position: {
+      x: 1,
+      y: 1,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-pawn.png`,
+    position: {
+      x: 2,
+      y: 1,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-pawn.png`,
+    position: {
+      x: 3,
+      y: 1,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-pawn.png`,
+    position: {
+      x: 4,
+      y: 1,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-pawn.png`,
+    position: {
+      x: 5,
+      y: 1,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-pawn.png`,
+    position: {
+      x: 6,
+      y: 1,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OUR,
+  },
+  {
+    image: `assets/images/white-pawn.png`,
+    position: {
+      x: 7,
+      y: 1,
+    },
+    type: PieceType.PAWN,
+    team: TeamType.OUR,
+  },
+];
---
+++ src/referee/referee.ts
@@ -0,0 +1,134 @@
+import { PieceType, TeamType, Piece, Position } from "../Constants";
+
+export default class Referee {
+
+    tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
+
+        const piece = boardState.find(p => p.position.x === x && p.position.y === y);
+        if (piece) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    tileisOccupiedByEnemy(x: number, y: number, boardState: Piece[], team: TeamType): boolean {
+        const piece = boardState.find(p => p.position.x === x && p.position.y === y && p.team !== team);
+
+        if (piece) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    isEnPassantMove(
+        initialPosition: Position,
+        desiredPosition: Position,
+        type: PieceType,
+        team: TeamType,
+        boardState: Piece[]){
+
+        const pawnDirection = team === TeamType.OUR ? 1 : -1;
+
+        if (type === PieceType.PAWN) {
+            if (
+              (desiredPosition.x - initialPosition.x === -1 ||
+                desiredPosition.x - initialPosition.x === 1) &&
+              desiredPosition.y - initialPosition.y === pawnDirection
+            ) {
+              const piece = boardState.find(
+                (p) =>
+                  p.position.x === desiredPosition.x &&
+                  p.position.y === desiredPosition.y - pawnDirection &&
+                  p.enPassant
+              );
+              if (piece) {
+                return true;
+              }
+            }
+          }
+        return false;
+    }
+
+ 
+
+    isValidMove(
+        initialPosition: Position,
+        desiredPosition: Position,
+        type: PieceType,
+        team: TeamType,
+        boardState: Piece[]
+      ) {
+        if (type === PieceType.PAWN) {
+          const specialRow = team === TeamType.OUR ? 1 : 6;
+          const pawnDirection = team === TeamType.OUR ? 1 : -1;
+    
+          //MOVEMENT LOGIC
+          if (
+            initialPosition.x === desiredPosition.x &&
+            initialPosition.y === specialRow &&
+            desiredPosition.y - initialPosition.y === 2 * pawnDirection
+          ) {
+            if (
+              !this.tileIsOccupied(
+                desiredPosition.x,
+                desiredPosition.y,
+                boardState
+              ) &&
+              !this.tileIsOccupied(
+                desiredPosition.x,
+                desiredPosition.y - pawnDirection,
+                boardState
+              )
+            ) {
+              return true;
+            }
+          } else if (
+            initialPosition.x === desiredPosition.x &&
+            desiredPosition.y - initialPosition.y === pawnDirection
+          ) {
+            if (
+              !this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)
+            ) {
+              return true;
+            }
+          }
+          //ATTACK LOGIC
+          else if (
+            desiredPosition.x - initialPosition.x === -1 &&
+            desiredPosition.y - initialPosition.y === pawnDirection
+          ) {
+            //ATTACK IN UPPER OR BOTTOM LEFT CORNER
+            console.log("upper / bottom left");
+            if (
+              this.tileisOccupiedByEnemy(
+                desiredPosition.x,
+                desiredPosition.y,
+                boardState,
+                team
+              )
+            ) {
+              return true;
+            }
+          } else if (
+            desiredPosition.x - initialPosition.x === 1 &&
+            desiredPosition.y - initialPosition.y === pawnDirection
+          ) {
+            //ATTACK IN THE UPPER OR BOTTOM RIGHT CORNER
+            console.log("upper / bottom right");
+            if (
+              this.tileisOccupiedByEnemy(
+                desiredPosition.x,
+                desiredPosition.y,
+                boardState,
+                team
+              )
+            ) {
+              return true;
+            }
+          }
+        }
+        return false;
+    }
+}
